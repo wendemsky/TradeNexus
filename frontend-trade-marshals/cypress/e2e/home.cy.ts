09f4f072193c172927369c719fe289ca53772e35
Aditi Anand <Anand.Aditi@fmr.com>
2024-09-05 10:41:38 +0530

    added e2e

--- frontend-trade-marshals/cypress/e2e/home.cy.ts
+++ frontend-trade-marshals/cypress/e2e/home.cy.ts
@@ -1,11 +1,40 @@
 describe('Home page', () => {
-    it('Validate the components of the Home page', () => {
-      cy.visit('/home');
-      cy.get(':nth-child(5) > .mdc-button__label').contains('Home')
-      cy.get('h1').contains('Welcome')
-      cy.get('.mat-mdc-nav-list > :nth-child(2)').should('exist')
-      cy.get('.mat-mdc-nav-list > :nth-child(4)').should('exist')
-      cy.get('.mat-mdc-nav-list > :nth-child(6)').should('exist')
-    })
+  beforeEach(()=>{
+    cy.visit('/home');
+  })
+
+  it('Validate the components of the Home page', () => {
+    cy.get(':nth-child(5) > .mdc-button__label').contains('Home')
+    cy.get('h1').contains('Welcome')
+    cy.get('.mat-mdc-nav-list > :nth-child(2)').should('exist')
+    cy.get('.mat-mdc-nav-list > :nth-child(4)').should('exist')
+    cy.get('.mat-mdc-nav-list > :nth-child(6)').should('exist')
+  })
+
+  it('Validate Portfolio holding page', () =>{
+    cy.get('.mat-mdc-nav-list > :nth-child(4)').click()
+    cy.get('.title').contains('Current Balance')
+    cy.get('.home-content > .ng-star-inserted > :nth-child(2)').contains('My Holdings')
+    cy.get('.ag-center-cols-viewport').should('exist')
+  })
+
+  it('Validate Trade Hidtory page', () =>{
+    cy.get('.mat-mdc-nav-list > :nth-child(6)').click()
+    cy.get('h2').contains('Trade History')
+    cy.get('.ag-center-cols-viewport').should('exist')
+  })
+
+  it('Validate Client Preferences page', () =>{
+    cy.get('.mat-mdc-nav-list > :nth-child(2)').click()
+    cy.get('h2').contains('CLIENT PREFERENCES')
+    cy.get('#mat-select-value-1').should('exist')
+    cy.get('#mat-select-value-3').should('exist')
+    cy.get('#mat-select-value-5').should('exist')
+    cy.get('#mat-select-value-7').should('exist')
+    cy.get('.mdc-slider__input').should('exist')
+    cy.get('.center-buttons > .mdc-button > .mdc-button__label').contains('Save').click()
+    cy.get('h1').contains('Welcome')
+
+  })
 })
   
--- frontend-trade-marshals/cypress/e2e/landing-page.cy.ts
+++ frontend-trade-marshals/cypress/e2e/landing-page.cy.ts
@@ -1,13 +1,15 @@
 describe('Landing page', () => {
-  it('Validate the Landing page of the application', () => {
+  beforeEach(() => {
     cy.visit('/');
+  })
+
+  it('Validate the Landing page of the application', () => {
     cy.get('.nav-title').contains('Trade Marshals');
     cy.get('.mdc-button__label > a').contains('Features')
     cy.get(':nth-child(6) > .mdc-button__label').contains('Login')
   })
 
   it("Validate the login of user", () =>{
-    cy.visit('/');
     cy.get(':nth-child(6) > .mdc-button__label').contains('Login').click();
     cy.get('#mat-input-0').type('john.doe@gmail.com')
     cy.get('#mat-input-1').click().type('Marsh2024')
