0e6f28dbc0438c11afe1311f0df31338891328ce
a752071 <K.Sowmya@fmr.com>
2024-09-23 09:56:27 +0530

    Adding ClientTradeDaoImplTest

--- backend-trade-marshals/src/test/java/com/fidelity/integration/ClientTradeDaoImplTest.java
+++ backend-trade-marshals/src/test/java/com/fidelity/integration/ClientTradeDaoImplTest.java
@@ -2,7 +2,9 @@ package com.fidelity.integration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.math.BigDecimal;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -11,6 +13,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.fidelity.models.ClientPortfolio;
+import com.fidelity.models.Order;
+import com.fidelity.models.Trade;
+import com.fidelity.models.TradeHistory;
 
 class ClientTradeDaoImplTest {
 	static PoolableDataSource dataSource;
@@ -63,6 +68,33 @@ class ClientTradeDaoImplTest {
 			dao.getClientPortfolio(clientId);
 		});
 		assertEquals("Client ID does not exist", e.getMessage());
+	}
+	
+	@Test
+	void testGetClientTradeHistory() {
+		String clientId = "1654658069";
+		TradeHistory tradeHistory = dao.getClientTradeHistory(clientId);
+		assertNotNull(tradeHistory);
+		assertTrue(tradeHistory.getTrades().size()>1);
+	}
+	
+	@Test
+	void testAddTrade() throws SQLException {
+		Order newOrder = new Order("NT123456", 1000, new BigDecimal(45.678), "B", "739982664", "ORDER020", 20 );
+		Trade newTrade = new Trade(newOrder, new BigDecimal(45.678), "TRADE020", new BigDecimal(45678));
+		int oldSize = DbTestUtils.countRowsInTable(dataSource.getConnection(), "CLIENT_TRADE");
+		dao.addTrade(newTrade);
+		int newSize = DbTestUtils.countRowsInTable(dataSource.getConnection(), "CLIENT_TRADE");
+		assertEquals(newSize, oldSize+1);
+	}
+	@Test
+	void testAddTradeExistingOrderId() {
+		Order newOrder = new Order("NT123456", 1000, new BigDecimal(45.678), "B", "739982664", "ORDER010", 20 );
+		Trade newTrade = new Trade(newOrder, new BigDecimal(45.678), "TRADE010", new BigDecimal(45678));
+ 
+		assertThrows(DatabaseException.class, ()->{
+			dao.addTrade(newTrade);
+		});
 	}
 	
 	
